73ddbae938dd541cfb8698dfe53d23717da8bb3a
Ramprit Sahani <rampritsahani@outlook.in>
2022-10-19 09:01:45 +0530

    change constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,7 @@
-export default {
+const Constants = {
   TOKEN_KEY: "access_token",
   AUTH_API_URL:
     "https://noco-db-production-35b3.up.railway.app/api/v1/auth/user",
   REST_API_URL: `https://noco-db-production-35b3.up.railway.app/api/v1/db/data/v1/dni/v1/dni`,
 };
+export default Constants;
